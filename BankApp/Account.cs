c30b3b9bb2823ed269e7857f55af71e15b5160d8
sfrease <sfrease@gmail.com>
2017-11-17 20:22:34 -0800

    Initial commit of bank app

---
+++ BankApp/Account.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankApp
+{
+    /// <summary>
+    /// This is about a bank account and can be written on multiple lines
+    /// </summary>
+    class Account
+    {
+        #region Properties
+        public int AccountNumber { get; private set; }
+        public string EmailAddress { get; set; }
+        public string AccountName { get; set; }
+        public string AccountType { get; set; }
+        public decimal Balance { get; private set; }
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Deposit money into your account
+        /// </summary>
+        /// <param name="amount">Amount to be deposited</param>
+        public void Deposit(decimal amount)
+        {
+            Balance += amount;
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            Balance -= amount;
+
+        }
+
+        #endregion
+
+
+
+
+    }
+}
---
+++ BankApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //reference, object, instance of a class
+            var account = new Account();
+            account.EmailAddress = "test@test.com";
+            account.AccountType = "Checking";
+
+            account.Deposit(100.10M);
+            Console.WriteLine($"{account.Balance}");
+
+        }
+    }
+}
